221657d23487bbf2fe957b9dbcc6547ad7c8e774
Anand Upadhyay <anandgupadhyay03@gmail.com>
2023-10-11 14:01:58 +0530

    First Commit

---
+++ Encryption/EncryptionApp.swift
@@ -0,0 +1,17 @@
+//
+//  EncryptionApp.swift
+//  Encryption
+//
+//  Created by User1 on 14/09/23.
+//
+
+import SwiftUI
+
+@main
+struct EncryptionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
